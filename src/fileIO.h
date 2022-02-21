73c625c35247692fa596c06f8fac197f494d6724
nmac-dev <ntwmac.dev@gmail.com>
2022-02-21 15:07:52 +0000

    Repo Repurposed

--- src/fileIO.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-/**      Function Prototypes      **/
-extern int *getInputFileValues(char *);
-void setupSingleOutput(char *, int *);
-void writeSingleOutput(char *, int *);
-void writeOutputFile(char *, int *, int *);
-FILE *validateFile(char *, char *);
--- src/simQ.h
+++
@@ -1,8 +0,0 @@
-#include "fileIO.h"
-#include "queueSystem.h"
-
-/*      Function Prototypes      */
-void runSims(int *, int *, int *, gsl_rng *, int, char *);
-int  genRandom(gsl_rng *, int, int);
-void setArrays(int (*)[2], int *, int, int);
-void sumOutputParams(int *outputParams, int numSims);

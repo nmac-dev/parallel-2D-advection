06a5328cd1d51ee1e5d1e71a747d3468dcb7c422
nmac-dev <ntwmac.dev@gmail.com>
2022-02-28 00:23:21 +0000

    Added parallel execution using OpenMP

--- src/advection2D.c
+++ src/advection2D.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+#include <omp.h>
 
 /// Prototypes
 float calc_vel_x(float height);
@@ -85,34 +86,46 @@ int main()
 	printf("Distance advected y = %g\n", VEL_Y * TIME_STEP * (float)N_STEPS);
 
 	/**
-	 * Place x points in the middle of the cell
+	 * 	WARNING: GCC 9 or older implements the changes made to default(none) in OpenMP 4.0
+	 *	default(none) NO LONGER predetermines "const variables with no mutable members" as shared (breaking backwards compatibility)
+	 *	To implement a program which compiles for any GCC version, any const variables will be declared as "firstprivate"
 	 */
-	/// LOOP 1
-	for (int i = 0; i < NX + 2; i++)
+	#pragma omp parallel default(none) shared(x_axis, y_axis, u_vals) \
+				firstprivate(NX, NY, DIST_X, DIST_Y, CENTER_X, CENTER_Y, SIGMA_X2, SIGMA_Y2)
 	{
-		x_axis[i] = ((float)i - 0.5f) * DIST_X;
-	}
+		/**
+		* Place x points in the middle of the cell
+		*/
+		/// LOOP 1
+		#pragma omp for
+		for (int i = 0; i < NX + 2; i++)
+		{
+			x_axis[i] = ((float)i - 0.5f) * DIST_X;
+		}
 
-	/**
-	 * Place y points in the middle of the cell
-	 */
-	/// LOOP 2
-	for (int j = 0; j < NY + 2; j++)
-	{
-		y_axis[j] = ((float)j - 0.5f) * DIST_Y;
-	}
-
-	/**
-	 * Set up Gaussian initial conditions
-	 */
-	/// LOOP 3
-	for (int i = 0; i < NX + 2; i++)
-	{
+		/**
+		* Place y points in the middle of the cell
+		*/
+		/// LOOP 2
+		#pragma omp for
 		for (int j = 0; j < NY + 2; j++)
 		{
-			sq_x2 = (x_axis[i] - CENTER_X) * (x_axis[i] - CENTER_X);
-			sq_y2 = (y_axis[j] - CENTER_Y) * (y_axis[j] - CENTER_Y);
-			u_vals[i][j] = exp(-1.0f * ((sq_x2 / (2.0f * SIGMA_X2)) + (sq_y2 / (2.0f * SIGMA_Y2))));
+			y_axis[j] = ((float)j - 0.5f) * DIST_Y;
+		}
+
+		/**
+		* Set up Gaussian initial conditions
+		*/
+		/// LOOP 3
+		#pragma omp for private(sq_x2, sq_y2)
+		for (int i = 0; i < NX + 2; i++)
+		{
+			for (int j = 0; j < NY + 2; j++)
+			{
+				sq_x2 = (x_axis[i] - CENTER_X) * (x_axis[i] - CENTER_X);
+				sq_y2 = (y_axis[j] - CENTER_Y) * (y_axis[j] - CENTER_Y);
+				u_vals[i][j] = exp(-1.0f * ((sq_x2 / (2.0f * SIGMA_X2)) + (sq_y2 / (2.0f * SIGMA_Y2))));
+			}
 		}
 	}
 
@@ -122,6 +135,9 @@ int main()
 	FILE *initial_file;
 	initial_file = fopen("initial.dat", "w");
 	/// LOOP 4
+	///	  No parallel implementation:
+	///		Writing to a singular file using multiple threads breaks output dependency
+	///		Each thread overwrites the other threads changes
 	for (int i = 0; i < NX + 2; i++)
 	{
 		for (int j = 0; j < NY + 2; j++)
@@ -132,57 +148,72 @@ int main()
 	fclose(initial_file);
 
 	/**
-	 * Update solution by looping over time steps
+	 * Process rows sequentially and columns in parallel
+	 *	placing the parallel block in the outer scope of the sequential loop reduces overhead for calling threads
 	 */
-	/// LOOP 5
-	for (int m = 0; m < N_STEPS; m++)
+	#pragma omp parallel default(none) shared(x_axis, y_axis, u_vals, roc_u, u_vad) \
+			firstprivate(NX, NY, DIST_X, DIST_Y, VEL_Y, N_STEPS, TIME_STEP, BVAL_LEFT, BVAL_RIGHT, BVAL_LOWER, BVAL_UPPER)
 	{
 		/**
-		 * Apply boundary conditions at u_vals[0][:] and u_vals[NX+1][:]
-		 */
-		/// LOOP 6
-		for (int j = 0; j < NY + 2; j++)
+		* Update solution by looping over time steps
+		*/
+		/// LOOP 5
+		///	  No parallel implementation (outer loop only) . . .
+		///		Each iteration of the outer loop depends upon the updates made by the previous
+		///		Flow dependency is broken in parallel as the time steps become unordered
+		for (int m = 0; m < N_STEPS; m++)
 		{
-			u_vals[0][j] = BVAL_LEFT;
-			u_vals[NX + 1][j] = BVAL_RIGHT;
-		}
-
-		/**
-		 * Apply boundary conditions at u_vals[:][0] and u_vals[:][NY+1]
-		 */
-		/// LOOP 7
-		for (int i = 0; i < NX + 2; i++)
-		{
-			u_vals[i][0] = BVAL_LOWER;
-			u_vals[i][NY + 1] = BVAL_UPPER;
-		}
-
-		/** 
-		 * Calculate rate of change of u using leftward difference
-		 * 	Loop over points in the domain but not boundary values
-		 */
-		/// LOOP 8
-		for (int i = 1; i < NX + 1; i++)
-		{
-			for (int j = 1; j < NY + 1; j++)
+			/**
+			* Apply boundary conditions at u_vals[0][:] and u_vals[NX+1][:]
+			*/
+			/// LOOP 6
+			#pragma omp for
+			for (int j = 0; j < NY + 2; j++)
 			{
-				roc_u[i][j] = -calc_vel_x(y_axis[j]) * (u_vals[i][j] - u_vals[i - 1][j]) / DIST_X - VEL_Y * (u_vals[i][j] - u_vals[i][j - 1]) / DIST_Y;
+				u_vals[0][j] = BVAL_LEFT;
+				u_vals[NX + 1][j] = BVAL_RIGHT;
 			}
-		}
 
-		/**
-		 * Update u from t to t+TIME_STEP & Calculate vertically averaged distribution
-		 * 	Loop over points in the domain but not boundary values
-		 */
-		/// LOOP 9
-		for (int i = 1; i < NX + 1; i++)
-		{
-			for (int j = 1; j < NY + 1; j++)
+			/**
+			* Apply boundary conditions at u_vals[:][0] and u_vals[:][NY+1]
+			*/
+			/// LOOP 7
+			#pragma omp for
+			for (int i = 0; i < NX + 2; i++)
 			{
-				u_vals[i][j] = u_vals[i][j] + roc_u[i][j] * TIME_STEP;
-				u_vad[i - 1] += u_vals[i][j];	// Sum vertical u values (y)
+				u_vals[i][0] = BVAL_LOWER;
+				u_vals[i][NY + 1] = BVAL_UPPER;
 			}
-			u_vad[i - 1] /= NY; // Divide sum by total points of y to get vertical average
+
+			/** 
+			* Calculate rate of change of u using leftward difference
+			* 	Loop over points in the domain but not boundary values
+			*/
+			/// LOOP 8
+			#pragma omp for
+			for (int i = 1; i < NX + 1; i++)
+			{
+				for (int j = 1; j < NY + 1; j++)
+				{
+					roc_u[i][j] = -calc_vel_x(y_axis[j]) * (u_vals[i][j] - u_vals[i - 1][j]) / DIST_X - VEL_Y * (u_vals[i][j] - u_vals[i][j - 1]) / DIST_Y;
+				}
+			}
+
+			/**
+			* Update u from t to t+TIME_STEP & Calculate vertically averaged distribution
+			* 	Loop over points in the domain but not boundary values
+			*/
+			/// LOOP 9
+			#pragma omp for
+			for (int i = 1; i < NX + 1; i++)
+			{
+				for (int j = 1; j < NY + 1; j++)
+				{
+					u_vals[i][j] = u_vals[i][j] + roc_u[i][j] * TIME_STEP;
+					u_vad[i - 1] += u_vals[i][j];	// Sum vertical u values (y)
+				}
+				u_vad[i - 1] /= NY; // Divide sum by total points of y to get vertical average
+			}
 		}
 	}
 
@@ -192,6 +223,7 @@ int main()
 	FILE *final_file;
 	final_file = fopen("final.dat", "w");
 	/// LOOP 10
+	///   No parallel implementation: same case as LOOP 4
 	for (int i = 0; i < NX + 2; i++)
 	{
 		for (int j = 0; j < NY + 2; j++)
@@ -207,6 +239,7 @@ int main()
 	FILE *v_averaged_file;
 	v_averaged_file = fopen("v_averaged.dat", "w");
 	/// LOOP 11
+	///   No parallel implementation: same case as LOOP 4
 	for (int i = 0; i < NX; i++)
 	{
 		fprintf(v_averaged_file, "%g %g\n", x_axis[i], u_vad[i]);
